b462ae4ba67faf2f8742d3aad8011337934c7dc9
Silviu Toderita <silviu.toderita@gmail.com>
2020-04-15 21:04:45 -0700

    Update server.js

--- node/server.js
+++ node/server.js
@@ -7,10 +7,6 @@ var floydSteinberg = require('floyd-steinberg');
 var fs = require('fs');
 var PNG = require('pngjs').PNG;
 
-function setBit(number, bitPosition) {
-  return number | (1 << bitPosition);
-}
-
 var mqtt_options = {
     port: 1883,
     username: 'silviu',
@@ -46,53 +42,51 @@ app.post( '/sms', ( req, res ) => {
 
         const request = http.get("http://ackwxpcapo.cloudimg.io/v7/" + req.body.MediaUrl0 + "?p=fax", function(response){
             response.pipe(new PNG()).on('parsed', function(){
-                floydSteinberg(this).pack().pipe(fs.createWriteStream('test.png'));
-            });
-        });
+                var ditherImage = floydSteinberg(this);
 
-        
-        
-        Jimp.read('test.png', function(err, image){
-            if(err){
-                console.log(err);
-            }else{
-                media += "w" + image.bitmap.width + "h" + image.bitmap.height;
-                console.log(media);
-                var size = image.bitmap.width * image.bitmap.height;
-      
-                var buffer = Buffer.alloc(size/8);
-                var bitCounter = 1;
-                var byteCounter = 0;
-                for(var i=1; i<=size; i++){
-                    if(image.bitmap.data[i] == 0){
-                        buffer[byteCounter] = setBit(0,bitCounter-1);
+                var size = ditherImage.width * ditherImage.height;
+
+                var buffer = Buffer.alloc(size/8 + 4);
+
+                buffer.writeUInt8(Math.floor(ditherImage.width/128), 0);
+                buffer.writeUInt8(ditherImage.width%128, 1);
+                buffer.writeUInt8(Math.floor(ditherImage.height/128), 2);
+                buffer.writeUInt8(ditherImage.height%128, 3);
+                
+                var byte = 0;
+                var bitCounter = 7;
+                var byteCounter = 4;
+                for(var i = 0; i < size * 4; i = i + 4){
+                    if(ditherImage.data[i] == 0){
+                        byte |= (1<<bitCounter);
                     }else{
-                        buffer[byteCounter] = setBit(1,bitCounter-1);
+                        byte |= (0<<bitCounter);
                     }
 
-                    if(bitCounter != 8){
-                        bitCounter++
-                    }else{
-                        bitCounter = 1;
+                    if(bitCounter == 0){
+                        bitCounter = 7;
+                        buffer.writeUInt8(byte, byteCounter);
+                        byte = 0;
                         byteCounter++;
+                    }else{
+                        bitCounter = bitCounter - 1;
                     }
-
                 }
 
-                fs.writeFile("/var/www/silviutoderita-com/output.txt", buffer, function(err){
+                fs.writeFileSync("/var/www/silviutoderita-com/output.txt", buffer, function(err){
                     if(err){
                         return console.log(err);
                     }
                     console.log("File Saved!");
-                })  
-
-            }
+                });  
 
             var mqtt_message = `id:${id}\nfrom:${from}\nbody:${body}\nmedia:${media}\ntime:${time}`;
 
             mqtt_client.publish(`smsin-${to}`, mqtt_message, {qos:1});
             console.log(`Published MQTT Message!\nTopic: smsin-${to} \nMessage:\n----\n${mqtt_message}\n----------------`);
+            });
 
+            
         });
 
     }else{
